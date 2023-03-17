ba103ef5e9ebc8b04fe81f13373e37d31d1f0f46
sfernandez <sfdezlop@gmail.com>
2023-03-17 03:18:41 +0100

    Add homa page

---
+++ src/components/home/home.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../header/header";
+import { Login } from "../login/login";
+
+export function Home() {
+  return (
+    <>
+      <Header></Header>
+      <Login></Login>
+    </>
+  );
+}
