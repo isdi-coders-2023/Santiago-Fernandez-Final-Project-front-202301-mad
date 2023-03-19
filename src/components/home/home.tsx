7bed0396a2c140b08d7701e624f22f1157e347dd
sfernandez <sfdezlop@gmail.com>
2023-03-19 11:05:16 +0100

    Bugfix code smells

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,13 +1,3 @@
-// import { Footer } from "../footer/footer";
-// import { Header } from "../header/header";
-// import { Login } from "../login/login";
-
 export function Home() {
-  return (
-    <>
-      {/* <Header></Header>
-      <Login></Login>
-      <Footer></Footer> */}
-    </>
-  );
+  return <></>;
 }
