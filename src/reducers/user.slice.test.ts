717da02114e314913f84a76cbe5e423b920249f0
sfernandez <sfdezlop@gmail.com>
2023-03-19 11:54:57 +0100

    Delete slice test

--- src/reducers/user.slice.test.ts
+++ src/reducers/user.slice.test.ts
@@ -1,30 +1,24 @@
-import { PayloadAction } from "@reduxjs/toolkit";
-import { UserStructure } from "../models/user.model";
-import { userReducer, UserStateStructure } from "./user.slice";
+export {};
+// import { PayloadAction } from "@reduxjs/toolkit";
+// import { userReducer, UserStateStructure } from "./user.slice";
 
-// const mockUser = {
-//   id: "1",
-//   email: "foo@foo.foo",
-//   passwd: "test",
+// const mockedInitialState: UserStateStructure = {
+//   userLoggedToken: "Sin Token",
+//   userLogged: {} as UserStructure,
+//   userLoggedMenuOptions: [],
+//   userLoggedInitials: "",
+//   users: [],
 // };
 
-const mockedInitialState: UserStateStructure = {
-  userLoggedToken: "Sin Token",
-  userLogged: {} as UserStructure,
-  userLoggedMenuOptions: [],
-  userLoggedInitials: "",
-  users: [],
-};
-
-describe("Given the userSlice", () => {
-  describe("When the method LOGIN is called", () => {
-    test("Then it should return in the state property loginToken the mocked token", () => {
-      const mockedAction: PayloadAction = {
-        payload: "test",
-        type: "users/loginToken",
-      };
-      const element = userReducer(mockedInitialState, mockedAction);
-      expect(element.userLoggedToken).toBe("test");
-    });
-  });
-});
+// describe("Given the userSlice", () => {
+//   describe("When the method LOGIN is called", () => {
+//     // test("Then it should return in the state property loginToken the mocked token", () => {
+//     //   const mockedAction: PayloadAction = {
+//     //     payload: "test",
+//     //     type: "users/loginToken",
+//     //   };
+//     //   const element = userReducer(mockedInitialState, mockedAction);
+//     //   expect(element.userLoggedToken).toBe("test");
+//     });
+//   });
+// });
