d96c5dfa8928970761447ba653bc27bc6f306dc0
sfernandez <sfdezlop@gmail.com>
2023-03-19 09:49:55 +0100

    Clean react counter files

---
+++ src/components/app/App.tsx
@@ -0,0 +1,16 @@
+import { AppRouter } from "../app.router/app.router";
+import { Footer } from "../footer/footer";
+import { Header } from "../header/header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <AppRouter></AppRouter>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
