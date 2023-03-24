57989d16dc3cfed2c51dd7ec53b634fc2ee42ca8
sfernandez <sfdezlop@gmail.com>
2023-03-24 05:37:01 +0100

    Add home and products pages

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,7 +1,51 @@
+// import { useEffect, useMemo } from "react";
+// import { useMemo } from "react";
+import { useSelector } from "react-redux";
+// import { useProducts } from "../../hooks/use.products";
+import { ProductStructure } from "../../models/product.model";
+// import { ProductsRepo } from "../../services/repositories/product.repo";
+import { RootState } from "../../store/store";
+import { Card } from "../../components/card/card";
+import { useEffect } from "react";
+
 export default function HomePage() {
+  // const repo = useMemo(() => new ProductsRepo(), []);
+  // const { gallery, loadCount, loadGallery } = useProducts(repo);
+  const galleryArray = useSelector(
+    (state: RootState) => state.productState.filteredGallery
+  );
+
+  // const firstName = useSelector(
+  //   (state: RootState) => state.userState.userLogged.firstName
+  // );
+
+  // useEffect(() => {
+  //   gallery();
+  // }, [galleryArray]);
+
+  useEffect(() => {}, [galleryArray]);
+
   return (
     <>
       <header>Home Page</header>
+      <ul className="homepage__list">
+        {galleryArray.map((item: Partial<ProductStructure>) => (
+          <Card key={item.id} product={item}></Card>
+        ))}
+      </ul>
     </>
   );
 }
+
+// return (
+//   <>
+//     <header>Home Page</header>
+//     <p>{firstName}</p>
+//     <p>{galleryArray[0].brand}</p>
+//     <ul className="homepage__list">
+//       {galleryArray.map((item: Partial<ProductStructure>) => (
+//         <li key={item.id}>{item.shortDescription}</li>
+//       ))}
+//     </ul>
+//   </>
+// );
--- src/pages/products/products.page.tsx
+++ src/pages/products/products.page.tsx
@@ -1,7 +1,24 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Card } from "../../components/card/card";
+import { ProductStructure } from "../../models/product.model";
+import { RootState } from "../../store/store";
+
 export default function ProductsPage() {
+  const galleryArray = useSelector(
+    (state: RootState) => state.productState.filteredGallery
+  );
+
+  useEffect(() => {}, [galleryArray]);
+
   return (
     <>
-      <header>Products Page</header>
+      <header>Product Gallery</header>
+      <ul className="productspage__list">
+        {galleryArray.map((item: Partial<ProductStructure>) => (
+          <Card key={item.id} product={item}></Card>
+        ))}
+      </ul>
     </>
   );
 }
